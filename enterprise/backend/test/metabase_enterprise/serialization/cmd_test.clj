d5fcdcdf2540ac7bba731210bb2f6c7103c95d1a
Cam Saul <github@camsaul.com>
2022-02-10 15:12:26 -0800

    Oops remove one more reference to the metabot group

--- enterprise/backend/test/metabase_enterprise/serialization/cmd_test.clj
+++ enterprise/backend/test/metabase_enterprise/serialization/cmd_test.clj
@@ -25,8 +25,7 @@
      (schema-migrations-test.impl/run-migrations-in-range! conn# [0 "v99.00-000"]) ; this should catch all migrations)
      ;; since the actual group defs are not dynamic, we need with-redefs to change them here
      (with-redefs [group/all-users (#'group/magic-group group/all-users-group-name)
-                   group/admin     (#'group/magic-group group/admin-group-name)
-                   group/metabot   (#'group/magic-group group/metabot-group-name)]
+                   group/admin     (#'group/magic-group group/admin-group-name)]
        ~@body)))
 
 (t/deftest no-collections-test
