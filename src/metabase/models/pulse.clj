2c45a2456b034ddc37ee6932f33d3451493cb740
TomGeorge <tg@incyan.com>
2022-03-07 10:55:47 +0000

    Revert "Allow super users to see all subscriptions"
    This reverts commit 786bb5b30afd22839b4cf89a082eccf86d01b934.

--- src/metabase/models/pulse.clj
+++ src/metabase/models/pulse.clj
@@ -314,14 +314,14 @@
 
 (s/defn retrieve-pulses :- [PulseInstance]
   "Fetch all `Pulses`."
-  [{:keys [archived? dashboard-id user-id is-superuser]
+  [{:keys [archived? dashboard-id user-id]
     :or   {archived? false}}]
   (let [query {:select    [:p.* [:%lower.p.name :lower-name]]
                :modifiers [:distinct]
                :from      [[Pulse :p]]
                :left-join (concat
                            [[:report_dashboard :d] [:= :p.dashboard_id :d.id]]
-                           (when-not is-superuser
+                           (when api/*current-user-id*
                              [[PulseChannel :pchan] [:= :p.id :pchan.pulse_id]
                               [PulseChannelRecipient :pcr] [:= :pchan.id :pcr.pulse_channel_id]]))
                :where     [:and
@@ -332,12 +332,12 @@
                             [:= :d.archived false]]
                            (when dashboard-id
                              [:= :p.dashboard_id dashboard-id])
-                           (when-not is-superuser
+                           (when api/*current-user-id*
                              [:and
                               [:not= :p.dashboard_id nil] 
                               [:or
-                               [:= :p.creator_id user-id]
-                               [:= :pcr.user_id user-id]]])]
+                               [:= :p.creator_id api/*current-user-id*]
+                               [:= :pcr.user_id api/*current-user-id*]]])]
                :order-by  [[:lower-name :asc]]}]
     (for [pulse (query-as Pulse query)]
       (-> pulse
